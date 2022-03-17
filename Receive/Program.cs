6094d607c31770902f30171d926608a677b61d74
Dário Domingos Praia (DBI) <udp02986@atlantico.ao>
2022-03-17 11:04:08 +0100

    first commit

---
+++ Receive/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+var factory = new ConnectionFactory() {
+    HostName = "127.0.0.1",
+    Port = 5672, //Protocols.DefaultProtocol.DefaultPort
+    UserName = "admin",
+    Password = "admin",
+};
+
+using (var connection = factory.CreateConnection())
+
+using (var channel = connection.CreateModel()) {
+    channel.QueueDeclare(
+        queue: "hello",
+        durable: false,
+        exclusive: false,
+        autoDelete: false,
+        arguments: null
+    );
+
+    var consumer = new EventingBasicConsumer(channel);
+    consumer.Received += (model, ea) => {
+        var body = ea.Body.ToArray();
+        var message = Encoding.UTF8.GetString(body);
+        Console.WriteLine($" [x] Received {message}");
+    };
+
+    channel.BasicConsume(
+        queue: "hello",
+        autoAck: true,
+        consumer: consumer
+    );
+
+    Console.ReadLine();
+}
---
+++ Send/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using RabbitMQ.Client;
+using System.Text;
+
+var factory = new ConnectionFactory() {
+    HostName = "127.0.0.1",
+    Port = 5672, //Protocols.DefaultProtocol.DefaultPort
+    UserName = "admin",
+    Password = "admin", 
+};
+
+using (var connection = factory.CreateConnection())
+
+using (var channel = connection.CreateModel()) {
+    channel.QueueDeclare(
+        queue: "hello",
+        durable: false,
+        exclusive: false,
+        autoDelete: false,
+        arguments: null
+    );
+
+    string message = "Hello World!";
+    var body = Encoding.UTF8.GetBytes(message);
+
+    channel.BasicPublish(
+        exchange: "",
+        routingKey: "hello",
+        basicProperties: null,
+        body: body
+    );
+
+    Console.WriteLine($" [x] Sent {message}");
+}
+
+// Console.WriteLine($" [x] Sent {0}", message);
+Console.ReadLine();
